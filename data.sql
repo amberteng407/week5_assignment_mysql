dd76ef4933ecbd3dd5c085878f8ecf33c406a278
amberteng407 <76562864+amberteng407@users.noreply.github.com>
2021-03-28 16:37:46 +0800

    Add files via upload

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.23, for macos10.15 (x86_64)
 --
--- Host: 127.0.0.1    Database: website
+-- Host: localhost    Database: website
 -- ------------------------------------------------------
 -- Server version	8.0.23
 
@@ -16,6 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `user_id` bigint NOT NULL COMMENT '留言會員編號',
+  `content` varchar(255) NOT NULL COMMENT '留言內容',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '留言時間',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,1,'this is test 1.','2021-03-28 15:13:12'),(2,2,'this is test 2.','2021-03-28 15:13:43'),(3,4,'這是測試3.','2021-03-28 15:14:21'),(4,1,'這是測試4.','2021-03-28 15:14:56');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -23,11 +51,11 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `name` varchar(255) NOT NULL COMMENT '姓名',
+  `username` varchar(255) NOT NULL COMMENT '帳戶名稱',
+  `password` varchar(255) NOT NULL COMMENT '帳戶密碼',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '註冊時間',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -51,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-27 22:34:54
+-- Dump completed on 2021-03-28 16:33:25
